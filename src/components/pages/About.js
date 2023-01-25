8075a7a30aade6d018154e55a4c8b48d36cc9173
92castro <celin92castro@gmail.com>
2023-01-25 14:42:13 -0700

    update about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -15,14 +15,12 @@ export default function About() {
         <img class="profilepic" src={Profilepic} alt="profile pic" />
         <p>
           I'll start out by saying, thank you. Thank you for taking the time for
-          visiting my portfolio that is still under construction. My name is
-          Celin Castro, and I was born in The Golden State-California, raised in
-          The Great State of Texas, and now I live in Colorful Colorado! I'm
-          currently a student at the University of Denver where I'm currently
-          attending a coding bootcamp to get ahead in life and find a career.
-          The reason I decided to built this page is to introduce myself to the
-          world of coding and to showoff my work. There's not much to show, but
-          I promise there's more to come!
+          visiting my portfolio. My name is Celin Castro, and I was born in The
+          Golden State-California, raised in The Great State of Texas, and now I
+          live in Colorful Colorado! I recently graduated from the University of
+          Denver where I completed an intensive coding bootcamp to get ahead in
+          life and find a career. The reason I decided to built this page is to
+          introduce myself to the world of coding and to showoff my work.
         </p>
       </div>
     </div>
